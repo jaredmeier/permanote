b82849038726d5e15f58577d79347d1263c4825a
Jared Meier <meier.jared@gmail.com>
2020-08-20 14:23:24 -0400

    Improve datetime for last updated with moment js

--- frontend/util/date_parse_util.js
+++ frontend/util/date_parse_util.js
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 export const getDate = (date) => {
     const d = new Date(date);
     return (
@@ -9,29 +11,23 @@ export const getDate = (date) => {
 export const getDateRelative = (date) => {
     const d = new Date(date);
     const n = new Date(Date.now());
+    
     if (n.getFullYear() === d.getFullYear()) {
         if (n.getMonth() === d.getMonth()) {
             if (n.getDate() === d.getDate()) {
-                const hours = n.getHours() - d.getHours();
-                const minutes = n.getMinutes() - d.getMinutes();
-                if (hours >= 1) { 
-                    return `${hours} hours ago`
-                } else if (minutes >= 5) {
-                    return "Less than an hour ago"
-                } else if (minutes >= 1) {
-                    return "A few minutes ago"
-                } else {
-                    return "Less than a minute ago"
-                }
-            } else if (n.getDate()) {
-                return "Yesterday"
+                const timeSince = moment(date).fromNow();
+                return timeSince.charAt(0).toUpperCase() + timeSince.slice(1);
+            } else if (n.getDate() === d.getDate() + 1) {
+                return "Yesterday";
+            } else if (n.getDate() < d.getDate() + 5) {
+                return moment(date).format('dddd');
             } else {
                 return ("".concat(d.toLocaleString('default', { month: 'short' }), " ",
-                    d.getDate()))
+                    d.getDate()));
             } 
         } else {
             return ("".concat(d.toLocaleString('default', { month: 'short' }), " ",
-                d.getDate()))
+                d.getDate()));
         }
     } else {
         return (
